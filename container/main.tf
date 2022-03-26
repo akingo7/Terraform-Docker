08eee5561065066d426a20a4cb094e3cc83254a2
akingo <akinmoyerogabriel@gmail.com>
2022-03-26 15:16:15 +0100

    Tested and workinh

--- container/main.tf
+++ container/main.tf
@@ -2,7 +2,7 @@ resource "docker_container" "container" {
   name  = var.name_in
   image = var.image_in
   ports {
-    external = "var.ext_port"
+    external = var.ext_port
     internal = var.int_port
   }
 
--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
 locals {
   development = {
     nodered = {
-      images   = var.images.nodered[terraform.workspace]
-      ext_port = keys(var.ext_ports.nodered[terraform.workspace])
+      images   = var.images["nodered"][terraform.workspace]
+      ext_port = var.ext_ports.nodered[terraform.workspace][0]
       int_port = 1880
     }
 
     grafana = {
-      images   = var.images.grafana[terraform.workspace]
-      ext_port = keys(var.ext_ports.grafana[terraform.workspace])
+      images   = var.images["grafana"][terraform.workspace]
+      ext_port = var.ext_ports.grafana[terraform.workspace][0]
       int_port = 3000
     }
 
     influxdb = {
-      images   = var.images.influxdb[terraform.workspace]
-      ext_port = keys(var.ext_ports.influxdb[terraform.workspace][*])
+      images   = var.images["influxdb"][terraform.workspace]
+      ext_port = var.ext_ports.influxdb[terraform.workspace][0]
       int_port = 8086
     }
   }
@@ -26,13 +26,17 @@ module "image" {
   for_each      = var.images
   image_name_in = each.value[terraform.workspace]
 }
-
-
+resource "random_string" "random" {
+  for_each = local.development
+  special  = false
+  upper    = false
+  length   = 4
+}
 module "container" {
   source   = "./container"
   for_each = local.development
-  name_in  = "${each.value.images}"
-  image_in = each.value.images
+  name_in  = join("-", [each.key, random_string.random[each.key].result])
+  image_in = module.image[each.key].image_out
   ext_port = each.value.ext_port
   int_port = each.value.int_port
 }
